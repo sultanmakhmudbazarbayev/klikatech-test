87f27b727d350ff92d093a9076085f8cdd0e9a47
sultanmakhmud <sultanmakhmudbazarbayev@gmail.com>
2023-02-12 05:00:25 +0600

    fix: add comments to music api

--- server/src/api/music/repository.js
+++ server/src/api/music/repository.js
@@ -6,18 +6,24 @@ exports.getMusic = async (pagination, filter) => {
   artistsFilter = {};
   genresFilter = {};
 
+  /// Add genre to object used to filtering
   if (filter.genre_id !== -1) {
     songsFilter.genre_id = filter.genre_id;
   }
+
+  /// Add year to object used to filtering
   if (filter.year !== -1) {
     songsFilter.year = +filter.year;
   }
+
+  /// Add song_name to object used to filtering
   if (filter.song_name !== -1) {
     songsFilter.song = {
       [Op.like]: '%' + filter.song_name + '%',
     };
   }
 
+  /// Add artist_name to object used to filtering
   if (filter.artist_name !== -1) {
     artistsFilter.artist_name = {
       [Op.like]: '%' + filter.artist_name + '%',
